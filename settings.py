156b11cfbc54b6eeb71b5161bb733110b1ab3fd3
00-hidan-00 <david.taranov@gmail.com>
2022-07-23 15:47:33 +0200

    :dizzy: Creat fuzz_generator.py

--- settings.py
+++ settings.py
@@ -1,2 +1,12 @@
-path = 'found_links.json'
-url = 'https://www.ted.com/talks/jay_walker_the_world_s_english_mania'
+import string
+from typing import Final
+
+path_for_links: Final[str] = 'found_links.json'
+path_for_words: Final[str] = 'found_words.json'
+url: Final[str] = 'https://www.ted.com/talks/jay_walker_the_world_s_english_mania'
+
+ALPHABET: Final[str] = ''.join([
+    string.ascii_lowercase,
+    string.ascii_uppercase,
+    string.digits,
+])
