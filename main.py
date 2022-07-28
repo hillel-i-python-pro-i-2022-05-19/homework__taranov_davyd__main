3b057e639529677ffd340dc78847f79ec7b0c969
00-hidan-00 <david.taranov@gmail.com>
2022-07-28 16:28:45 +0200

    :toolbox: edit fuzz_generator

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fuzz_generator import FuzzGenerator, _write_json_words_file
 
 
-def main(word_count, word_length):
-    args = args()
-    fuzz_generator = FuzzGenerator(word_count, word_length)
+def run(*args):
+    arg = args()
+    fuzz_generator = FuzzGenerator()
     words_list = fuzz_generator.world_generator()
     _write_json_words_file(path='words.json', words=words_list)
     fuzz_generator.start()
 
 
 if __name__ == '__main__':
-    main()
+    run()
