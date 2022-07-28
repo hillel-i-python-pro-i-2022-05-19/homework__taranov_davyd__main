ea59e1c99914042aae01d009d8ca2973346e0f25
00-hidan-00 <david.taranov@gmail.com>
2022-07-28 16:40:54 +0200

    :toolbox: edit fuzz_generator

--- tools/tools.py
+++ tools/tools.py
@@ -3,12 +3,14 @@ import argparse
 
 def args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--wc",
-                        "-word_count",
+    parser.add_argument("-wc",
+                        "--word_count",
                         type=int,
-                        default=100)
-    parser.add_argument("--wl"
-                        "-word_length",
+                        default=100,
+                        help='Word count')
+    parser.add_argument("-wl",
+                        "--word_length",
                         type=int,
-                        default=5)
+                        default=5,
+                        help="World length")
     return parser.parse_args()
