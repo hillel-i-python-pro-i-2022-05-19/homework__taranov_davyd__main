d1c10d644747b428f9cc6b647c694c48bec6659c
00-hidan-00 <david.taranov@gmail.com>
2022-08-06 14:38:41 +0200

    :toolbox: edit crawler.py

--- app/crawler.py
+++ app/crawler.py
@@ -15,10 +15,11 @@ class Crawler:
         self.urls_as_list: T_URLS = [input_url_as_str]
         self.max_number_of_urls = max_number_of_urls
         self.crawling_depth = crawling_depth
+        self.html_text_as_list: T_HTML_TEXTS = []
 
-    async def get_text_from_url(self, html_text_as_list: T_HTML_TEXTS):
+    async def get_text_from_url(self) -> None:
         index_for_while = -1
-        logging.info(f'start, amount of html text: {len(html_text_as_list)}')
+        logging.info(f'start, amount of html text: {len(self.html_text_as_list)}')
         with contextlib.suppress(TypeError):
             while True:
                 await asyncio.sleep(0.003)
@@ -29,16 +30,16 @@ class Crawler:
                     try:
                         async with session.get(self.urls_as_list[index_for_while]) as response:
                             response_text = await response.text()
-                            html_text_as_list.append(response_text)
-                            logging.info(f'amount of html text: {len(html_text_as_list)}')
+                            self.html_text_as_list.append(response_text)
+                            logging.info(f'amount of html text: {len(self.html_text_as_list)}')
                     except IndexError:
                         index_for_while -= 1
                         continue
                     if index_for_while == self.crawling_depth - 1:
                         break
-            logging.info(f'end, amount of html text: {len(html_text_as_list)}')
+            logging.info(f'end, amount of html text: {len(self.html_text_as_list)}')
 
-    async def get_urls_as_list(self, html_text_as_list: T_HTML_TEXTS):
+    async def get_urls_as_list(self) -> None:
         index_for_while = -1
         while_bool = True
         logging.info(f'start, amount of urls: {len(self.urls_as_list)}')
@@ -46,7 +47,7 @@ class Crawler:
             await asyncio.sleep(0.003)
             index_for_while += 1
             try:
-                soup = bs4.BeautifulSoup(markup=html_text_as_list[index_for_while], features='html.parser')
+                soup = bs4.BeautifulSoup(markup=self.html_text_as_list[index_for_while], features='html.parser')
             except IndexError:
                 index_for_while -= 1
                 continue
@@ -61,15 +62,14 @@ class Crawler:
                         logging.info(f"New url: {url_element.get('href')}")
             except TypeError:
                 continue
-            if self.crawling_depth == len(html_text_as_list):
+            if self.crawling_depth == len(self.html_text_as_list):
                 break
         logging.info(f'end, amount of urls: {len(self.urls_as_list)}')
 
     async def main(self):
-        html_text_as_list = []
         tasks = [
-            Crawler.get_text_from_url(self, html_text_as_list),
-            Crawler.get_urls_as_list(self, html_text_as_list)
+            Crawler.get_text_from_url(self),
+            Crawler.get_urls_as_list(self)
         ]
         await asyncio.gather(*tasks)
         logging.info(f'end, urls={len(self.urls_as_list)}')
